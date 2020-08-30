e0d034f04128e9aca75cb13deff32f9c0fcaf360
Revangga007 <revanggaajip2@gmail.com>
2020-08-30 13:54:13 +0700

    Configurasi template

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Head -->
+<?php include 'layouts/head.php' ?>
+<!-- End of Head -->
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php include 'layouts/sidebar.php' ?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <?php include 'layouts/topbar.php' ?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <?php include 'layouts/footer.php' ?>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <?php include 'layouts/scroll-top.php' ?>
+
+  <!-- Logout Modal-->
+  <?php include 'layouts/logout-modal.php' ?>
+
+  <!-- script -->
+  <?php include 'layouts/script.php' ?>
+  <!-- End of Script -->
+
+
+
+</body>
+
+</html>
